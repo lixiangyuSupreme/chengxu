294225bd9f5e0b8efa130a689390363aae8e6703
李项宇 <45160965+lixiangyuSupreme@users.noreply.github.com>
2020-10-29 18:16:52 +0800

    Update zuoye.js

--- zuoye.js
+++ zuoye.js
@@ -1,21 +1,9 @@
 'use strict'
 
 const titbit = require('titbit')
-// const {cookie,session} = require('titbit-toolkit')
 const app = new titbit({
     debug:true
 })
-
-// app.use((new cookie()).mid())
-
-//一定要再cookie之后，session是基于cookie的
-// app.use((new session()).mid())
-
-// app.use(async (c,next) => {
-//     console.log('just for home')
-//     await next()
-//     //
-// },{group: 'home'})
 
 app.get('/',async c => {
     c.res.body = 'lixiangyu'+' '+ 'success'
@@ -24,40 +12,5 @@ app.get('/',async c => {
 app.get('/info',async c => {
     c.res.body ='{"name":"lixiangyu","sno":"2018011802"}'
 })
-
-
-// app.post('/login',async c => {
-
-//     //提交数据解析到c.body
-//     console.log(c.body)
-
-//     c.res.body = c.body
-// })
-// app.use(async (c,next) => {
-    
-//     //设置最大提交允许10字节
-//     // c.maxBody = 10
-//     await next()
-//     //设置pre为true等效于使用app.pre，提供method指定针对哪些请求方法执行
-// },{pre:true,method:['POST','PUT']})
-
-
-
-// app.post('/test',async c => {
-    
-//     let{username,passwd} = c.body
-    
-//     if (username === 'testuser' && passwd === '123456'){
-//         c.setSession('is_login',true)
-//         //跳转成功直接到首页
-//         c.status(301)
-//         c.setHeader('Location','/')
-//     }else{
-//         //
-//         c.status(301)
-//         c.setHeader('Location','/login')
-//     }
-// })
-
 app.run(1236)
 
